c9499ac746ecc8669c494b45621dc1b52ca8395b
Antonyeaster <>
2024-01-06 13:44:53 +0000

    Add other components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,23 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import StatefulGreeting from "./components/StatefulGreeting"
+import NavBarSimple from "./components/NavBarSimple"
+import EventsClass from './components/EventsClass';
+import EventsFunctional from './components/EventsFunctional';
+import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 
 function App() {
   return (
     <div className={App}>
       {/* <Sidebar /> */}
-      <StatefulGreeting greeting="from the stateful component class!" name="Antony"/>
+      {/* <StatefulGreeting greeting="from the stateful component class!" name="Antony"/> */}
+      {/* <NavBarSimple /> */}
+      {/* <EventsFunctional />
+      <EventsClass /> */}
+      {/* <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional connected={false} /> */}
+      <NestingComponents />
     </div>
   );
 }
